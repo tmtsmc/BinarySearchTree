e703cf30154714421228ad9a1934b566473e1a37
tmtsmc <152163233+tmtsmc@users.noreply.github.com>
2023-11-27 22:33:13 +0100

    Add files via upload

---
+++ Arbre.c
@@ -0,0 +1,230 @@
+// ------------------------
+//
+//    PROJET ARBRE B46
+//
+//     Anon Anon
+//	   tmtsmc tmtsmc
+//
+// ------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>		// strcmp, strcpy
+#include <Windows.h> 	// clear
+
+// Arbre
+typedef struct noeud{
+	char mot[20];
+	struct noeud *fg,*fd;
+} liste_noeud;
+
+typedef liste_noeud *DICO;
+
+FILE *f;
+DICO pROOT = NULL;
+
+// Entêtes
+int lookup(char mot[20], DICO d);	//Recherche la présence d'un mot dans le dictionnaire
+int comparer(DICO a, DICO b);		//Compare deux arbres
+DICO insert(char mot[20], DICO d);	//Permet l'insertion d'un mot dans le dictionnaire
+void afficherDico(DICO d);			//Affiche les mots du dictionnaire
+void Menu();						//Affiche le menu
+void MenuComparer();				//Affiche le menu pour la comparaison
+void bufferManager();
+
+int main(int argc, char *argv[]) {
+	int x = 0, q = 1, u = 1, y = 0;
+	char mot[20];
+	DICO A = NULL, B = NULL;
+	f = fopen("mini_dico.txt","r+");
+	if (f == NULL) {
+		fprintf(stderr,"Erreur ouverture de fichier !\n");
+		return -1;
+	}
+	while (!feof(f)) {
+		fscanf(f, "%s", mot);
+		pROOT = insert(mot, pROOT);
+	
+	}
+	while (q) {
+		Menu();
+		//Saisie sécurisée.
+		while (!scanf("%d", &x)) {
+			printf(">Veuillez entrer un choix correct !\nChoix : ");
+			bufferManager();
+		}
+		bufferManager();
+		switch (x) {
+			case 1 : //insertion d'un élément
+				printf("sélectionnez un mot:\n");
+				//Saisie sécurisée.
+				while (!scanf("%30[a-z]s", mot)) {
+					printf(">Veuillez entrer un choix correct !\n> ");
+					bufferManager();
+				}
+				pROOT = insert(mot, pROOT);
+				if (lookup(mot, pROOT)) printf("Le mot a été inseré dans le dictionnaire\n");
+				else printf("Le mot n'a pas pu être inséré\n");
+				bufferManager();
+				getchar();
+				system("cls");
+				break;
+			case 2 : //Vérifier la présence d'un mot
+				printf("sélectionnez un mot:\n");
+				//Saisie sécurisée.
+				while (!scanf("%30[a-z]s", mot)) {
+					printf(">Veuillez entrer un choix correct !\n> ");
+					bufferManager();
+				}
+				if (lookup(mot, pROOT)) printf("Le mot est présent\n");
+				else printf("Le mot n'est pas présent\n");
+				bufferManager();
+				getchar();
+				system("cls");
+				break;
+			case 3 : //Affichage Dictionnaire
+				afficherDico(pROOT);
+				getchar();
+				system("cls");
+				break;
+			case 4 : //Deuxième menu.
+				while (u) {
+					system("cls");
+					MenuComparer();
+					//Saisie sécurisée.
+					while (!scanf("%d", &y)) {
+						printf(">Veuillez entrer un choix correct !\nChoix : ");
+						bufferManager();
+					}
+					bufferManager();
+					switch (y) {
+						case 1: //inserer un mot dans Dico A
+							printf("Selectionnez un mot:\n");
+							//Saisie sécurisée.
+							while (!scanf("%30[a-z]s", mot)) {
+								printf(">Veuillez entrer un choix correct !\n> ");
+								bufferManager();
+							}
+							A = insert(mot, A);
+							bufferManager();
+							system("cls");
+							break;
+						case 2: //inserer un mot dans Dico B
+							printf("Selectionnez un mot:\n");
+							//Saisie sécurisée.
+							while (!scanf("%30[a-z]s", mot)) {
+								printf(">Veuillez entrer un choix correct !\n> ");
+								bufferManager();
+							}
+							B = insert(mot, B);
+							bufferManager();
+							system("cls");
+							break;
+						case 3: //Afficher le Dico A
+							afficherDico(A);
+							getchar();
+							system("cls");
+							break;
+						case 4: //Afficher le Dico B
+							afficherDico(B);
+							getchar();
+							system("cls");
+							break;
+						case 5:	//comparaison entre Dico A et Dico B
+							if (comparer(A, B)) printf("les dictionnaires sont identiques\n");
+							else printf("Les dictionnaires ne sont pas identiques\n");
+							getchar();
+							system("cls");
+							break;
+						case 6: //Retour au premier menu.
+							u = 0;
+							system("cls");
+							break;
+						}
+					}
+				break;
+			case 5 :
+				q = 0;
+				break;
+		}
+	}
+	return 0;
+}
+
+// Fonction de recherche
+int lookup(char mot[20], DICO d) {
+	if (d == NULL) return 0;
+	if (strcmp(d->mot, mot) == 0) return 1;
+	if (strcmp(d->mot, mot) > 0) return lookup(mot, d->fg);
+	if (strcmp(d->mot, mot) < 0) return lookup(mot, d->fd);
+	return 0;
+}
+
+// Comparaison des deux dictionnaires
+int comparer(DICO a, DICO b){
+	if (a == NULL && b == NULL) return 1;
+	if ((a == NULL && b) || (a && b == NULL)) return 0;
+	else return (strcmp(a->mot, b->mot) == 0) && comparer(a->fg, b->fg) && comparer(a->fd, b->fd);	
+}
+
+// Insertion d'un élémént
+DICO insert(char mot[20], DICO d) {
+	if (d == NULL) {
+		DICO p = (DICO) malloc(sizeof (liste_noeud));
+		strcpy(p->mot, mot);
+		p->fg = NULL;
+		p->fd = NULL;
+		return p;
+	} else if (strcmp(mot, d->mot) < 0) {
+		d->fg=insert( mot, d->fg);
+		return d;
+	} else if(strcmp(mot, d->mot) > 0) {
+		d->fd=insert(mot, d->fd);
+		return d;
+	} else return d;
+}
+
+// Affichage d'un des dictionnaires
+void afficherDico(DICO d) {
+	if (d) {
+		afficherDico(d->fg);
+		printf("%s \n",d->mot);
+		afficherDico(d->fd);
+	}
+}
+
+// Menu principal
+void Menu() {
+	printf("                           |                        |\n");
+    printf("                           |     PROJET TP4 B46     |\n");
+    printf("                           |                        |\n");
+    printf("                           |     Anon & tmtsmc     |\n");
+    printf("                           |________________________|\n\n");
+	printf("Sélectionnez une option:\n\n");
+	printf("1. Insérer un élement\n");
+	printf("2. Vérifier la présence d'un mot\n");
+	printf("3. Afficher Dictionnaire\n");
+	printf("4. Comparer deux dictionnaire\n");
+	printf("5. Quitter\n\n");
+}
+
+// Menu secondaire
+void MenuComparer() {
+	printf("                           |                        |\n");
+    printf("                           |     PROJET TP4 B46     |\n");
+    printf("                           |                        |\n");
+    printf("                           |     Anon & tmtsmc     |\n");
+    printf("                           |________________________|\n\n");
+	printf("1. Inserer un mot dans le Dictionnaire A\n");
+	printf("2. Inserer un mot dans le Dictionnaire B\n");
+	printf("3. Afficher le Dictionnaire A\n");
+	printf("4. Afficher le Dictionnaire B\n");
+	printf("5. Comparer les Dictionnaires\n");
+	printf("6. Quitter la comparaison\n\n");
+}
+
+// Vide le buffer.
+void bufferManager() {
+    scanf("%*[^\n]"); // Se sert du regex pour gérer le buffer.
+    getchar();
+}
---
+++ Hachage.c
@@ -0,0 +1,227 @@
+// ------------------------
+//
+//   PROJET HACHAGE B46
+//
+//     Anon Anon
+//	   tmtsmc tmtsmc
+//
+// ------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>		// strcmp, strcpy
+#include <Windows.h>	// clear.
+
+#define C 1000
+
+// Liste chainée
+typedef struct Maillon {
+	char mot[30];
+	struct Maillon *pSuiv;
+} list_maillon;
+
+typedef list_maillon *LISTE;
+
+FILE *f;
+LISTE T[C];
+
+// Entêtes
+int hachage(char mot[30]);
+LISTE supprimer(char mot[30], LISTE A);
+LISTE Insert(char mot[30], LISTE L);
+int NbElements(LISTE L);
+void Affichage(LISTE L);
+int Element(char mot[30], LISTE L);
+void Menu();
+void bufferManager();
+
+// Créé à la présentation du projet
+void Compteur(LISTE A) {
+	LISTE crt;
+	printf("La classe contient %d elements");
+}
+
+int main(int argc, char *argv[]) {
+	char mot[30];
+	int i = 0, h = 0, x = 0, q = 1, nbr = 0;
+	/*f = fopen("mini_dico.txt", "r+");
+	if (!f) {
+		fprintf(stderr,"Erreur ouverture de fichier !\n");
+		return -1;
+	}
+	while (!feof(f)) {
+		fscanf(f,"%s", mot);
+		h = hachage(mot);
+		T[h] = Insert(mot, T[h]);
+	}*/
+	while (q) {
+		Menu();
+		//Saisie sécurisée.
+		while (!scanf("%d", &x)) {
+			printf(">Veuillez entrer un choix correct !\nChoix : ");
+			bufferManager();
+		}
+		bufferManager();
+		switch (x) {
+			case 0 : // fin du programme.
+				q = 0;
+				break;
+			case 1 : //insertion d'un élément
+				printf("Sélectionnez un mot: \n");
+				//Saisie sécurisée.
+				while (!scanf("%30[a-z]s", mot)) {
+					printf(">Veuillez entrer un choix correct !\n> ");
+					bufferManager();
+				}
+				h = hachage(mot);
+				T[h] = Insert(mot, T[h]);
+				bufferManager();
+				system("cls");
+				break;
+			case 2 : //Vérifier la présence d'un mot
+				printf("Sélectionnez un mot: \n");
+				//Saisie sécurisée.
+				while (!scanf("%30[a-z]s", mot)) {
+					printf(">Veuillez entrer un choix correct !\n> ");
+					bufferManager();
+				}
+				h = hachage(mot);
+				if (Element(mot, T[h])) printf("%s est présent dans le dictionnaire\n", mot);
+				else printf("%s n'est pas présent dans le dictionnaire\n", mot);
+				bufferManager();
+				getchar();
+				system("cls");
+				break;
+			case 3:	//Affichage Dictionnaire
+				for (i = 0; i < C; i++) Affichage(T[i]);
+				getchar();
+				system("cls");
+				break;
+			case 4: //Nombre d'élements
+				for (i = 0; i < C; i++) nbr += NbElements(T[i]);
+				printf(">Il y a %d elements dans le dictionnaire", nbr);
+				nbr = 0;
+				getchar();
+				system("cls");
+				break;
+			case 5:	//Supprimer un élément.
+				printf("Supprimer un mot: \n");
+				//Saisie sécurisée.
+				while (!scanf("%30[a-z]s", mot)) {
+					printf(">Veuillez entrer un choix correct !\n> ");
+					bufferManager();
+				}
+				h = hachage(mot);
+				T[h] = supprimer(mot, T[h]);
+				printf("L'element %s a ete supprime", mot);
+				bufferManager();
+				getchar();
+				system("cls");
+				break;
+			case 6 :
+				for (i = 0; i < C; i++) printf("Il y a %d elements\n",  NbElements(T[i]));
+				getchar();
+				system("cls");
+				break;
+		}
+	}
+	return 0;
+}
+
+// Fonction de Hachage
+int hachage(char mot[30]) {
+	int i = 0, x = 0;
+	for (i = 0; mot[i] != '\0'; i++) x += mot[i];
+	x %= C;
+	if (x < 0) return x + C;
+	return x;
+}
+
+// Supprime l'élément de la liste identique à la chaîne de caratères passée en argument.
+LISTE supprimer(char mot[30], LISTE A) {
+	LISTE crt, prc;
+    crt = prc = A;
+    while (crt) {
+        if (!strcmp(crt->mot, mot)) {
+            if (prc == crt) A = crt->pSuiv;
+            else prc->pSuiv = crt->pSuiv;
+            free(crt->mot);
+            free(crt);
+            return A;
+        } else {
+            prc = crt;
+            crt = crt->pSuiv;
+        }
+    }
+    printf(">Element non trouve.\n");
+    return A;
+}
+
+/*LISTE Insert(char mot[30], LISTE L) { // Ne gère pas les doublons.
+	LISTE temp = (LISTE) malloc(sizeof(list_maillon));
+	strcpy(temp->mot, mot);
+	temp->pSuiv = L;
+	return temp;
+}*/
+
+LISTE Insert(char mot[30], LISTE L) { // Gère les doublons
+	int a = 1;
+	LISTE temp = L;
+	while (temp) {
+		if (!strcmp(mot, temp->mot)) a = 0;
+		temp = temp->pSuiv;
+	}
+	if (a) {
+		temp = (LISTE) malloc(sizeof(list_maillon));
+		strcpy(temp->mot, mot);
+		temp->pSuiv = L;
+		return temp;
+	} else {
+		return L;
+	}
+}
+
+int NbElements(LISTE L) {
+	int cpt = 0;
+	while (L) {
+		cpt++;
+		L = L->pSuiv;
+	}
+	return cpt;
+}
+
+void Affichage(LISTE L) {
+	if (L) {
+		printf("%s\n", L->mot);
+		Affichage(L->pSuiv);
+	}
+}
+
+int Element(char mot[30], LISTE L) {
+	while (L) {
+		if (strcmp(L->mot,mot) == 0) return 1;
+		L = L->pSuiv;
+	}
+	return 0;
+}
+
+void Menu() {
+	printf("                           |                        |\n");
+    printf("                           |   PROJET HACHAGE B46   |\n");
+    printf("                           |                        |\n");
+    printf("                           |     Anon & tmtsmc     |\n");
+    printf("                           |________________________|\n\n");
+	printf("Sélectionnez une option:\n\n");
+	printf("0. Quitter\n");
+	printf("1. Insérer un élement\n");
+	printf("2. Vérifier la présence d'un mot\n");
+	printf("3. Afficher Dictionnaire\n");
+	printf("4. Nombre d'élément dans le dictionnaire\n");
+	printf("5. Supprimer un element\n");
+}
+
+// Vide le buffer.
+void bufferManager() {
+    scanf("%*[^\n]"); // Se sert du regex pour gérer le buffer.
+    getchar();
+}
